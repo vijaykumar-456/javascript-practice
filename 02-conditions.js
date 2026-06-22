8a5c9d6259728fd929a7fbfe2abf060eb02490ba
Vk167 <viijaykumarr07@gmail.com>
2026-06-22 17:24:25 +0530

    javascript practice

--- 02-conditions.js
+++ 02-conditions.js
@@ -9,3 +9,8 @@ if (age < 18 ) {
     console.log(" You are above 18 and below 60")
 }
 // console.log(man)
+
+let age_no = 16;
+
+let result = age_no >= 18 ? "adult" : " NOt Adult";
+console.log(result)
