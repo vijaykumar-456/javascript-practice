a82a5f83a9e4e497bcc3cdbe86aa89603eadbacb
Vk167 <viijaykumarr07@gmail.com>
2026-06-22 17:20:20 +0530

    javascript practice

---
+++ 02-conditions.js
@@ -0,0 +1,11 @@
+let age = 35;
+let man;
+
+if (age < 18 ) {
+    console.log("You are below Voter Line")
+} else if (age > 60 ){
+    console.log("You are above Voter Line")  
+} else {
+    console.log(" You are above 18 and below 60")
+}
+// console.log(man)
