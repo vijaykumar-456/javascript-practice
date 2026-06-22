7a45879950574c6c97991a92f94b9c640ab4ccd2
Vk167 <viijaykumarr07@gmail.com>
2026-06-22 22:03:00 +0530

    javascript practice

---
+++ 03-prompts.js
@@ -0,0 +1,8 @@
+let num = prompt("Enter a number here");
+console.log(num);
+
+if (num%5 === 0){
+    console.log("It is a multiple of 5");
+} else {
+    console.log("It is not a multiple of 5");
+}
---
+++ 04-loops.js
@@ -0,0 +1,28 @@
+// For Loop
+
+for (let count = 1; count < 5; count++){
+    console.log("Apna college")
+}
+console.log("loop ended")
+
+let sum=0;
+for  (let i=0; i <= 5; i++){
+    sum = sum+i;
+}
+
+console.log(sum)
+
+
+// While loop
+let i = 1;
+while (i <= 5){
+    console.log("i=",i);
+    i++;
+}
+
+// do-while loop
+let j = 20;
+do {
+    console.log("Apna college");
+    j++;
+} while ( j <= 10);
