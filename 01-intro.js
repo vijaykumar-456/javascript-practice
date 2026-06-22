65bdccc8939405cfd8c01db55c2b240ec99dc89b
Vk167 <viijaykumarr07@gmail.com>
2026-06-22 15:45:51 +0530

    javascript practice

---
+++ 01-intro.js
@@ -0,0 +1,20 @@
+// console.log("Hello World");
+
+// const Product = {
+//     objName: "Parker Jotter",
+//     Rating: 4,
+//     Price: 270,
+//     Discount: "5%"
+// };
+
+// console.log(Product)
+
+
+let a = 5;
+let b = 2;
+
+console.log("a + b = ", a + b);
+a++;
+console.log(a);
+
+
